4d4496f7fe64326e7e0c889908e350802654017f
Milo MacPhail <milomacphail@gmail.com>
2018-10-17 18:59:07 +0000

    Added logging middleware

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-   response.send("Hello, World");
-})
+var logger = require('./logger');
+app.use(logger);
 
-app.get('/blocks', function(request, response) {
-   var blocks = ['Fixed','Moveable','Rotating'];
-})
+app.use(express.static('public'));
 
-app.get('/blocks', function(request, response) {
-   response.redirect(301, '/parts');
+app.get('/blocks', function(request, response){
+    var blocks = ["Fixed", "Movable", "Rotating"];
+    response.json(blocks);
 });
 
-
 app.listen(process.env.PORT, process.env.IP, 8080, function(){
-    console.log("Listening on port 8080");
+    console.log("You are listening on port 8080");
 });
 
 
